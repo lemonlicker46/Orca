50bb67d4446136b79e7ff0da14889a47b4b2b763
lemonlicker46 <lemonlicker46@gmail.com>
2026-05-27 18:20:57 +0100

    Add files via upload
    cleaned up make.cmd by removing IMG2CARRAY build process from make.cmd

--- Win32/make.cmd
+++ Win32/make.cmd
@@ -2,7 +2,7 @@
 REM Simple batch Makefile for Orca
 setlocal
 if not exist build mkdir build
-powershell -NoProfile -Command "& { $sw=[diagnostics.stopwatch]::StartNew(); & gcc 'src\main.c' -o 'build\Orca.exe' -mwindows -lcomctl32; if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }; & gcc 'src\img2carray.c' -o 'build\IMG2CARRAY.exe'; $code=$LASTEXITCODE; $sw.Stop(); if ($code -ne 0) { Write-Host 'Build failed.'; Write-Host ('Elapsed: {0} ms' -f $sw.Elapsed.TotalMilliseconds.ToString('F3')); exit $code } else { Write-Host 'Build succeeded.'; Write-Host ('Elapsed: {0} ms' -f $sw.Elapsed.TotalMilliseconds.ToString('F3')); exit 0 } }"
+powershell -NoProfile -Command "& { $sw=[diagnostics.stopwatch]::StartNew(); & gcc 'src\main.c' -o 'build\Orca.exe' -mwindows -lcomctl32; if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }; $code=$LASTEXITCODE; $sw.Stop(); if ($code -ne 0) { Write-Host 'Build failed.'; Write-Host ('Elapsed: {0} ms' -f $sw.Elapsed.TotalMilliseconds.ToString('F3')); exit $code } else { Write-Host 'Build succeeded.'; Write-Host ('Elapsed: {0} ms' -f $sw.Elapsed.TotalMilliseconds.ToString('F3')); exit 0 } }"
 cmd /k
 if %errorlevel% neq 0 (
   exit /b %errorlevel%
